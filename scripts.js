5f2dc217e24e146a0df121c893630aae631a6f7a
jdobbs34 <dobbsjason34@yahoo.com>
2025-12-03 17:51:10 -0700

    1st commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// what do we know
+
+
+
+
+// let plantSpace = 0.8
+// let totalPlants = 20
+// let oneWeek = totalPlants
+
+// console.log("totalPlants * oneWeek", totalPlants * oneWeek);
+
+let beginingPlants = 20;
+let plantSpace = 0.8; 
+let totalPlantSpace = beginingPlants * plantSpace
+console.log(beginingPlants * plantSpace)
+
